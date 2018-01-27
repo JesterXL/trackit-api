6ac59e87a78bfa3fdbaf9995252cee3d6c7810b1
Jesse Warden <jesterxl@jessewarden.com>
2018-01-27 11:57:42 -0500

    trying restify

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const log = console.log;
 log("Starting Fastify...");
-const fastify = require('fastify')()
+// const fastify = require('fastify')()
 
-fastify.get('/', (request, reply) => {
-  reply.send({ result: true })
-})
-fastify.get('/api/ping', (request, reply) => {
-  reply.send({ result: true, data: 'pong' })
-})
+// fastify.get('/', (request, reply) => {
+//   reply.send({ result: true })
+// })
+// fastify.get('/api/ping', (request, reply) => {
+//   reply.send({ result: true, data: 'pong' })
+// })
 
-const port = process.env.PORT || 3000;
-log("Port:", port);
-fastify.listen(port,  (err) => {
-  if (err) throw err
-  console.log(`server listening on ${fastify.server.address().port}`)
+// const port = process.env.PORT || 3000
+// log("Port:", port)
+// fastify.listen(port,  (err) => {
+//   if (err) throw err
+//   console.log(`server listening on ${fastify.server.address().port}`)
+// })
+
+const restify = require('restify');
+
+var server = restify.createServer();
+server.get('/', (req, res) => {
+  res.send({result: true})
+});
+server.get('/api/ping', (req, res) => {
+  res.send({result: true, data: 'pong'});
+});
+const port = process.env.PORT || 3000
+log("Port:", port)
+server.listen(port, function() {
+  console.log('%s listening at %s', server.name, server.url);
 });
